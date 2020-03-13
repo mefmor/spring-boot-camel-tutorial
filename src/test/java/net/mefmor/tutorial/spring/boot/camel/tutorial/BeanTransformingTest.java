5ce37dbb67270fb4235c4820a30ee1ad80d279b5
Pavel Zaitsev <Pavel_Zaitsev@epam.com>
2020-03-13 14:13:56 +0300

    Change test runner for pure apache camel

--- src/test/java/net/mefmor/tutorial/spring/boot/camel/tutorial/BeanTransformingTest.java
+++ src/test/java/net/mefmor/tutorial/spring/boot/camel/tutorial/BeanTransformingTest.java
@@ -1,42 +1,18 @@
 package net.mefmor.tutorial.spring.boot.camel.tutorial;
 
-import org.apache.camel.CamelContext;
-import org.apache.camel.EndpointInject;
-import org.apache.camel.Produce;
-import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.test.spring.junit5.CamelSpringTest;
-import org.junit.jupiter.api.BeforeEach;
+import org.apache.camel.test.junit5.CamelTestSupport;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTestContextBootstrapper;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.BootstrapWith;
-import org.springframework.test.context.ContextConfiguration;
 
-@CamelSpringTest
-@ContextConfiguration
-@BootstrapWith(SpringBootTestContextBootstrapper.class)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-class BeanTransformingTest {
-    @Autowired
-    private CamelContext context;
-
-    @Produce("direct:xml")
-    private ProducerTemplate template;
-
-    @EndpointInject("mock:result")
-    private MockEndpoint mock;
-
-    @BeforeEach
-    void setupRoute() throws Exception {
-        context.addRoutes(new RouteBuilder() {
+class BeanTransformingTest extends CamelTestSupport {
+    @Override
+    protected RouteBuilder createRouteBuilder() {
+        return new RouteBuilder() {
             @Override
-            public void configure() throws Exception {
+            public void configure() {
                 from("direct:xml").bean(new TransformBean()).to("mock:result");
             }
-        });
+        };
     }
 
     private static class TransformBean {
@@ -47,10 +23,10 @@ class BeanTransformingTest {
 
     @Test
     void inputAndOutput() throws InterruptedException {
-        mock.expectedBodiesReceived("Hello, Boris!");
+        getMockEndpoint("mock:result").expectedBodiesReceived("Hello, Boris!");
 
-        template.sendBody("Boris");
+        template.sendBody("direct:xml", "Boris");
 
-        mock.assertIsSatisfied();
+        assertMockEndpointsSatisfied();
     }
 }
--- src/test/java/net/mefmor/tutorial/spring/boot/camel/tutorial/TokenizeXmlSplitTest.java
+++ src/test/java/net/mefmor/tutorial/spring/boot/camel/tutorial/TokenizeXmlSplitTest.java
@@ -1,53 +1,30 @@
 package net.mefmor.tutorial.spring.boot.camel.tutorial;
 
-import org.apache.camel.CamelContext;
-import org.apache.camel.EndpointInject;
-import org.apache.camel.Produce;
-import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.test.spring.junit5.CamelSpringTest;
-import org.junit.jupiter.api.BeforeEach;
+import org.apache.camel.test.junit5.CamelTestSupport;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTestContextBootstrapper;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.BootstrapWith;
-import org.springframework.test.context.ContextConfiguration;
 
-@CamelSpringTest
-@ContextConfiguration
-@BootstrapWith(SpringBootTestContextBootstrapper.class)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-class TokenizeXmlSplitTest {
-    @Autowired
-    private CamelContext context;
 
-    @Produce("direct:xml")
-    private ProducerTemplate template;
-
-    @EndpointInject("mock:result")
-    private MockEndpoint mock;
-
-    @BeforeEach
-    void setupRoute() throws Exception {
-        context.addRoutes(new RouteBuilder() {
+class TokenizeXmlSplitTest extends CamelTestSupport {
+    @Override
+    protected RouteBuilder createRouteBuilder() {
+        return new RouteBuilder() {
             @Override
-            public void configure() throws Exception {
+            public void configure() {
                 from("direct:xml")
                         .split().tokenizeXML("Info")
                         .to("mock:result");
             }
-        });
+        };
     }
 
     @Test
     void inputAndOutput() throws InterruptedException {
-        mock.expectedBodiesReceived("<Info id='1'/>", "<Info id='2'/>");
+        getMockEndpoint("mock:result").expectedBodiesReceived("<Info id='1'/>", "<Info id='2'/>");
 
-        template.sendBody("<XML><Batch><Info id='1'/><Info id='2'/></Batch></XML>");
+        template.sendBody("direct:xml", "<XML><Batch><Info id='1'/><Info id='2'/></Batch></XML>");
 
-        mock.assertIsSatisfied();
+        assertMockEndpointsSatisfied();
     }
 
 }
