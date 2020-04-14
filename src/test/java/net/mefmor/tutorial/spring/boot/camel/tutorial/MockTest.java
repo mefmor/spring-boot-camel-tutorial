1757296d6308e1fb338f432c2512875c87b8f634
Pavel Zaitsev <Pavel_Zaitsev@epam.com>
2020-04-14 10:52:29 +0300

    Add test expected body

--- src/test/java/net/mefmor/tutorial/spring/boot/camel/tutorial/MockTest.java
+++ src/test/java/net/mefmor/tutorial/spring/boot/camel/tutorial/MockTest.java
@@ -3,9 +3,12 @@ package net.mefmor.tutorial.spring.boot.camel.tutorial;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.junit5.CamelTestSupport;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MockTest extends CamelTestSupport {
+
+    private MockEndpoint mock;
 
     protected RouteBuilder createRouteBuilder() {
         return new RouteBuilder() {
@@ -16,13 +19,27 @@ public class MockTest extends CamelTestSupport {
         };
     }
 
+    @BeforeEach
+    void mockEndpointSetup() throws Exception {
+        mock = getMockEndpoint("mock:quote");
+    }
+
     @Test
-    void testQuote() throws InterruptedException {
-        MockEndpoint quote = getMockEndpoint("mock:quote");
-        quote.expectedMessageCount(1);
+    void testExpectedMessageCount() throws InterruptedException {
+        mock.expectedMessageCount(1);
 
         template.sendBody("stub:jms:topic:quote", "Camel rocks");
 
-        quote.assertIsSatisfied();
+        mock.assertIsSatisfied();
+    }
+
+    @Test
+    void testExpectedBodies() throws InterruptedException {
+        mock.expectedBodiesReceived("Camel rocks", "Hello Camel");
+
+        template.sendBody("stub:jms:topic:quote", "Camel rocks");
+        template.sendBody("stub:jms:topic:quote", "Hello Camel");
+
+        mock.assertIsSatisfied();
     }
 }
