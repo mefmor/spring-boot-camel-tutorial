74fb7e00492c80cc8a27294a874c2a3461b349da
Pavel Zaitsev <Pavel_Zaitsev@epam.com>
2020-04-30 08:35:18 +0300

    Add additional tests for DefaultErrorHandlerRedeliveryTest

--- src/test/java/net/mefmor/tutorial/spring/boot/camel/tutorial/error/handling/DefaultErrorHandlerRedeliveryTest.java
+++ src/test/java/net/mefmor/tutorial/spring/boot/camel/tutorial/error/handling/DefaultErrorHandlerRedeliveryTest.java
@@ -5,6 +5,8 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.test.junit5.CamelTestSupport;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class DefaultErrorHandlerRedeliveryTest extends CamelTestSupport {
     @Override
     protected RouteBuilder createRouteBuilder() {
@@ -31,6 +33,30 @@ public class DefaultErrorHandlerRedeliveryTest extends CamelTestSupport {
         getMockEndpoint("mock:first").whenExchangeReceived(1, e -> e.setException(new Exception("Some exception")));
 
         template.sendBody("direct:start", "Message");
+
+        assertMockEndpointsSatisfied();
+    }
+
+    @Test
+    void redeliveryHappensAtThePointOfError() throws InterruptedException {
+        getMockEndpoint("mock:first").expectedMessageCount(1);
+        getMockEndpoint("mock:second").expectedMessageCount(2);
+
+        getMockEndpoint("mock:second").whenExchangeReceived(1, e -> e.setException(new Exception("Some exception")));
+
+        template.sendBody("direct:start", "Message");
+
+        assertMockEndpointsSatisfied();
+    }
+
+    @Test
+    void afterAllAttemptsYouGetAnException() throws InterruptedException {
+        getMockEndpoint("mock:first").expectedMessageCount(1);
+        getMockEndpoint("mock:second").expectedMessageCount(3);
+
+        getMockEndpoint("mock:second").whenAnyExchangeReceived(e -> e.setException(new Exception("Some exception")));
+
+        assertThrows(Exception.class, () -> template.sendBody("direct:start", "Message"));
 
         assertMockEndpointsSatisfied();
     }
