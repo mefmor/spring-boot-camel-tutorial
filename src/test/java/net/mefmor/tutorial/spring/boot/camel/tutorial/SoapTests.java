2ed1d91511b37f78c013b91d990852d207a12524
Pavel Zaitsev <Pavel_Zaitsev@epam.com>
2020-11-18 12:16:11 +0300

    Add marshal example

--- src/test/java/net/mefmor/tutorial/spring/boot/camel/tutorial/SoapTests.java
+++ src/test/java/net/mefmor/tutorial/spring/boot/camel/tutorial/SoapTests.java
@@ -3,6 +3,8 @@ package net.mefmor.tutorial.spring.boot.camel.tutorial;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.spring.ws.SpringWebserviceConstants;
 import org.apache.camel.test.junit5.CamelTestSupport;
+import org.example.Add;
+import org.example.ObjectFactory;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +15,7 @@ public class SoapTests extends CamelTestSupport {
             @Override
             public void configure() throws Exception {
                 from("direct:xml")
+                        .marshal().jaxb(Add.class.getPackage().getName())
                         .setHeader(SpringWebserviceConstants.SPRING_WS_SOAP_ACTION, simple("http://Example.org/ICalculator/Add"))
                         .to("spring-ws:http://127.0.0.1:8080/ICalculator");
             }
@@ -22,8 +25,11 @@ public class SoapTests extends CamelTestSupport {
     @Test
     @Disabled("For using with SOAP UI")
     void testSendMessageAdd() {
+        Add add = new ObjectFactory().createAdd();
+        add.setA(2);
+        add.setB(3);
 
-        template.sendBody("direct:xml", "<exam:Add xmlns:exam=\"http://Example.org\" />");
+        template.sendBody("direct:xml", add);
 
     }
 }
