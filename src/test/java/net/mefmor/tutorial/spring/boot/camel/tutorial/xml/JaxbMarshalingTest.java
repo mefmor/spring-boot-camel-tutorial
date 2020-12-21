5899e0be41ecba64d3019237d4f6d3239f3c11ff
Pavel Zaitsev <Pavel_Zaitsev@epam.com>
2020-12-21 11:40:47 +0300

    Refactor JaxbMarshalingTest

--- src/test/java/net/mefmor/tutorial/spring/boot/camel/tutorial/xml/JaxbMarshalingTest.java
+++ src/test/java/net/mefmor/tutorial/spring/boot/camel/tutorial/xml/JaxbMarshalingTest.java
@@ -5,15 +5,10 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JaxbDataFormat;
 import org.apache.camel.test.junit5.CamelTestSupport;
 import org.junit.jupiter.api.Test;
-import org.springframework.core.io.DefaultResourceLoader;
-import org.springframework.core.io.Resource;
 import org.springframework.util.FileCopyUtils;
 import org.xmlunit.assertj.XmlAssert;
 
 import java.io.InputStreamReader;
-import java.io.Reader;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
 
 
 public class JaxbMarshalingTest extends CamelTestSupport {
@@ -22,26 +17,25 @@ public class JaxbMarshalingTest extends CamelTestSupport {
         return new RouteBuilder() {
             @Override
             public void configure() {
-                JaxbDataFormat jaxb = new JaxbDataFormat();
-                jaxb.setContextPath("net.mefmor.tutorial.spring.boot.camel.tutorial.xml");
-
                 from("direct:object")
-                        .marshal(jaxb)
+                        .marshal().jaxb(Client.class.getPackage().getName())
                         .to("mock:xml");
             }
         };
     }
 
     @Test
-    void transformObjectToXml() {
-        String expectedOutput = asString("expected_client.xml");
+    void transformObjectToXml() throws InterruptedException {
+        getMockEndpoint("mock:xml").expectedMessagesMatches((exchange) -> {
+            String input = exchange.getIn().getBody(String.class);
+            XmlAssert.assertThat(input).and(asString("expected_client.xml"))
+                    .ignoreComments().ignoreWhitespace().areIdentical();
+            return true;
+        });
 
         template.sendBody("direct:object", Client.builder().name("Boris").build());
 
-        String actualOutput = getMockEndpoint("mock:xml").getExchanges().get(0).getIn().getBody(String.class);
-
-        XmlAssert.assertThat(actualOutput).and(expectedOutput)
-                .ignoreComments().ignoreWhitespace().areIdentical();
+        assertMockEndpointsSatisfied();
     }
 
     @SneakyThrows
